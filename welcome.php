bdd4077836da9b129a09aff8d83251bc4ff14cf1
Lwalker79 <lwalker79@student.cccs.edu>
2022-11-30 19:29:18 -0700

    AAA
    AAAa

---
+++ welcome.php
@@ -0,0 +1,9 @@
+<html>
+<body>
+
+Welcome <?php echo $_GET["name"]; ?><br>
+Your Email:<?php echo $_GET["email"]; ?><br>
+Thank you for your Feedback! <?php echo $_GET["feedback"]; ?>
+
+</body>
+</html> 
